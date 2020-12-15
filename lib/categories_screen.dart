35823e3cefb11b3ec0ea6e5c8ac0a4ee7e15cfe0
Thinh Le <letienthinh0109@gmail.com>
2020-12-16 00:00:50 +0700

    7.4 - add children to GridView by map forom Category to CategoryItem

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:meals_app/category_item.dart';
+import 'dummy_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //we can able to use .builder with the list with dynamic amount => but no
     // need
     return GridView(
+      children: (DUMMY_CATEGORIES
+          .map((cate) => (CategoryItem(cate.title, cate.color)))).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2, //200 width => 300 height
@@ -14,7 +19,6 @@ class CategoriesScreen extends StatelessWidget {
       //SliverGridDelegateWithMaxCrossAxisExtent() delegate size for EACH
       // item in GridView and the GridView try to add as many columns as
       // possible
-      children: [],
     );
   }
 }
