ba5dd1602cab1917031bd859fa6bc974034dcd76
Thinh Le <letienthinh0109@gmail.com>
2020-12-16 12:59:32 +0700

    7.22 - config TabScreen with some setting, change the Screen list to a List of Map

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -9,27 +9,47 @@ class TabsScreen extends StatefulWidget {
 
 //have to build like this => flutter do auto things for you
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {"page": CategoriesScreen(), "title": "Categories"},
+    {"page": FavoritesScreen(), "title": "Favorites"},
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Meals"),
-          bottom: TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.category), text: "Categories"),
-              Tab(icon: Icon(Icons.star), text: "Favorites"),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meals"),
+      ),
+      body: _pages[_selectedPageIndex]["page"],
+      //use have to control manually, not automatically like the
+      // DefaultTabBarView...
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        onTap: _selectPage,
+        //Flutter automatically give u the index ?? wtf bit of cumbersome here
+        backgroundColor: Theme.of(context).primaryColor,
+        currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            label: _pages[_selectedPageIndex]["title"],
           ),
-        ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            label: _pages[_selectedPageIndex]["title"],
+          ),
+        ],
       ),
     );
   }
